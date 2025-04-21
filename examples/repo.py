cc11ae60a0461a05830ea85e4f1d74b9be54f199
AmFlint <antoine.masselot@astronomer.io>
2025-04-21 17:41:21 +0200

    update metadata

--- examples/repo.py
+++ examples/repo.py
@@ -33,7 +33,7 @@ import requests
     catchup=False,
     doc_md=__doc__,
     default_args={"owner": "Astro", "retries": 3},
-    tags=["example"],
+    tags=["example", "antoine"],
 )
 def example_astronauts():
     # Define tasks
